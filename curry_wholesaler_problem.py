244f1cc6f2e16f39c4e2f47f2422e49bbb9a96f9
Wayne Hartigan <hartiganwayne@gmail.com>
2021-11-16 10:25:15 +0000

    Push current code

---
+++ curry_wholesaler_problem.py
@@ -0,0 +1,37 @@
+import fileinput
+
+
+def format_file_lines(curry_preferences):
+    list_of_lines = []
+    for line in curry_preferences:
+        if not curry_preferences.isfirstline():
+            line = line.strip().replace(" ", "")
+            list_of_lines.append([line[index: index + 2] for index in range(0, len(line), 2)])
+    return list_of_lines
+
+
+def sort_curry_choices(curry_preferences):
+    curry_choices = []
+    list_of_pref = format_file_lines(curry_preferences)
+    for pref in list_of_pref:
+        for choice in pref:
+            if choice not in curry_choices:
+                if "M" in choice:
+                    if len(pref) == 1:
+                        curry_choices.append(choice)
+                else:
+                    curry_choices.append(choice)
+
+    ans = ""
+    for choice in curry_choices:
+        ans = ans + choice[1]
+    print(ans)
+    return ans
+
+
+if __name__ == "__main__":
+    """
+    python3 curry_wholesaler_problem.py test_files/test_1.txt
+    """
+    curry_preferences = fileinput.input()
+    sort_curry_choices(curry_preferences)
---
+++ test_curry_wholesaler_problem.py
@@ -0,0 +1,24 @@
+import fileinput
+from curry_wholesaler_problem import sort_curry_choices
+
+
+class TestCurryWholesaler:
+    def test_1(self):
+        input_file = fileinput.input("test_files/test_1.txt")
+        resp = sort_curry_choices(input_file)
+        assert resp == "VVVVM"
+
+    def test_2(self):
+        input_file = fileinput.input("test_files/test_2.txt")
+        resp = sort_curry_choices(input_file)
+        assert resp == "No solution exists"
+
+    def test_3(self):
+        input_file = fileinput.input("test_files/test_3.txt")
+        resp = sort_curry_choices(input_file)
+        assert resp == "VMVMV"
+
+    def test_4(self):
+        input_file = fileinput.input("test_files/test_4.txt")
+        resp = sort_curry_choices(input_file)
+        assert resp == "VM"

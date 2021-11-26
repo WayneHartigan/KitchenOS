395c9ab8c9d334c5016cfacbcab1bbb04841791c
Wayne Hartigan <hartiganwayne@gmail.com>
2021-11-26 23:23:16 +0000

    Use regex to accept double digit indexs & add extra test_case

--- curry_wholesaler_problem.py
+++ curry_wholesaler_problem.py
@@ -1,38 +1,48 @@
 import fileinput
+import re
 
 
-def format_file_lines(curry_preferences):
+def format_file_lines(file):
     """
-    :param curry_preferences: file_object
+    :param file: file_object
     :return: list_of_choices: {1: 'MM', 2: 'VV'}
     """
     list_of_choices = {}
-    for line in curry_preferences:
+    for line in file:
         line = line.strip().replace(" ", "")
-        if not curry_preferences.isfirstline():
-            for curry_choice in [line[index: index + 2] for index in range(0, len(line), 2)]:
+        choices = re.findall(r'(\d+[VM])', line)
+        if not file.isfirstline():
+            for curry_choice in choices:
+                index = int(curry_choice[:-1])
                 try:
-                    list_of_choices[curry_choice[0]] = list_of_choices[curry_choice[0]] + curry_choice[1]
+                    list_of_choices[index] = list_of_choices[index] + curry_choice[-1]
                 except KeyError:
-                    list_of_choices[curry_choice[0]] = curry_choice[1]
+                    list_of_choices[index] = curry_choice[-1]
     return list_of_choices
+
+
+def check_for_no_solution(answer, max_length):
+    if len(answer) == 0 or len(answer) != max_length:
+        answer = "No solution exists"
+    return answer
 
 
 def sort_curry_choices(curry_preferences):
     curry_choices = {}
+    max_length = int(curry_preferences.readline())
     list_of_pref = format_file_lines(curry_preferences)
     for key, pref in list_of_pref.items():
-        if "M" not in pref:
-            curry_choices[key] = "V"
-        if "V" not in pref:
-            curry_choices[key] = "M"
+        if "M" not in pref or "V" not in pref:
+            curry_choices[key] = pref[0]
         else:
-            pass
-            # Logic for finding most efficient
+            for _, current_pick in sorted(curry_choices.items()):
+                if current_pick == "M":
+                    curry_choices[key] = "V"
+
     ans = ""
     for key, pref in sorted(curry_choices.items()):
         ans = ans + pref
-    return ans
+    return check_for_no_solution(ans, max_length)
 
 
 if __name__ == "__main__":
@@ -40,4 +50,4 @@ if __name__ == "__main__":
     python3 curry_wholesaler_problem.py test_files/test_1.txt
     """
     curry_preferences = fileinput.input()
-    sort_curry_choices(curry_preferences)
+    print(sort_curry_choices(curry_preferences))
--- test_curry_wholesaler_problem.py
+++ test_curry_wholesaler_problem.py
@@ -6,7 +6,7 @@ class TestCurryWholesaler:
     def test_1(self):
         input_file = fileinput.input("test_files/test_1.txt")
         resp = sort_curry_choices(input_file)
-        assert resp == "VVVVM"
+        assert resp == "MVVVV"
 
     def test_2(self):
         input_file = fileinput.input("test_files/test_2.txt")
@@ -20,5 +20,10 @@ class TestCurryWholesaler:
 
     def test_4(self):
         input_file = fileinput.input("test_files/test_4.txt")
+        resp = sort_curry_choices(input_file)
+        assert resp == "VM"
+
+    def test_5(self):
+        input_file = fileinput.input("test_files/test_5.txt")
         resp = sort_curry_choices(input_file)
         assert resp == "VM"

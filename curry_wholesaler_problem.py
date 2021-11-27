302f8fa7920d214c82f25bbaf35643a44acd0286
Wayne Hartigan <hartiganwayne@gmail.com>
2021-11-27 22:27:37 +0000

    Added parameterized tests & comments

--- curry_wholesaler_problem.py
+++ curry_wholesaler_problem.py
@@ -5,7 +5,8 @@ import re
 def format_file_lines(file):
     """
     :param file: file_object
-    :return: list_of_choices: {1: 'MM', 2: 'VV'}
+    :return: list_of_choices: [(1, 'MM'), (2, 'VV')]
+    Formats file correctly to be checked
     """
     list_of_choices = {}
     for line in file:
@@ -21,6 +22,14 @@ def format_file_lines(file):
 
 
 def check_for_no_solution(answer, max_length):
+    """
+    :param answer: "VVMVV"
+    :param max_length: 5
+    :return: answer
+    """
+    # If returned ans == "" or
+    # If the length of the answer is not equal to the required curries
+    # return "No solution exists"
     if len(answer) == 0 or len(answer) != max_length:
         answer = "No solution exists"
     return answer
@@ -28,14 +37,22 @@ def check_for_no_solution(answer, max_length):
 
 def sort_curry_choices(curry_preferences):
     ans = ""
+    # Get the max length from first line
+    # This also reads the first line, will not be read by rest of code
     max_length = int(curry_preferences.readline().strip())
     list_of_pref = format_file_lines(curry_preferences)
+
+    # Loop through each number to check the preferred curry
     for _, pref in list_of_pref:
+        # If preference is only one curry choose that curry
+        # E.g "MMM" == "M" or "VV" == "V"
         if "M" not in pref or "V" not in pref:
             ans = ans + pref[0]
         else:
+            # If pref == "VM" and only 1 can be chosen, no solution exists
             if len(pref) == 2 and max_length == 1:
                 return "No solution exists"
+            # Otherwise V can be assumed to be correct
             ans = ans + "V"
     return check_for_no_solution(ans, max_length)
 
--- test_curry_wholesaler_problem.py
+++ test_curry_wholesaler_problem.py
@@ -1,8 +1,24 @@
 import fileinput
+from parameterized import parameterized
 from curry_wholesaler_problem import sort_curry_choices
 
 
-class TestCurryWholesaler:
+class TestCurryWholesalerProblem:
+    @parameterized.expand(
+        [
+            ("test_files/test_1.txt", "MVVVV"),
+            ("test_files/test_2.txt", "MVVVV"),
+            ("test_files/test_3.txt", "MVVVV"),
+            ("test_files/test_4.txt", "MVVVV"),
+            ("test_files/test_5.txt", "MVVVV"),
+        ]
+    )
+    def test_curry_wholesaler_problem(self, test_file, expected):
+        input_file = fileinput.input(test_file)
+        resp = sort_curry_choices(input_file)
+        assert resp == expected
+
+    # Below tests if prefer not to use parameterized testing
     def test_1(self):
         input_file = fileinput.input("test_files/test_1.txt")
         resp = sort_curry_choices(input_file)
